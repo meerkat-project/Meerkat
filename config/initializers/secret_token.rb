2f46623047e9a5f9a4bbda829bf1946fc6ad165e
adlermeerkat <adler.meerkat@gmail.com>
2013-05-28 19:35:40 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meerkat::Application.config.secret_token = '8b1f027f650d388b4988a1fa71cd0d5c8d9493304209a54a004a57cde34408b0f6d8ca5a95ac49ef76c65a222122e86e887db1611693574e5ef2c942ee4ae4d4'
