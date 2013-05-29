817f29322d20d7c3ac79aa18386863470e7ee969
adlermeerkat <adler.meerkat@gmail.com>
2013-05-29 11:57:09 +0200

    Basic Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Meerkat::Application.routes.draw do
+
+
+  #Static Pages
+  match '/', to: 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,7 @@ end
 
 
 
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -58,6 +59,7 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
