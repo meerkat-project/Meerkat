0775790993902fb7b25f75f4801ffe960958fd48
adlermeerkat <adler.meerkat@gmail.com>
2013-06-01 16:41:15 +0200

    Finished making basic User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,10 @@
 #This is our USER MODEL - People's information will be stored here.
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :username
+  attr_accessible :email, :username, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email=email.downcase}
 
 
 #Validations
@@ -21,4 +24,6 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: {maximum: 15}, format: {with: VALID_USERNAME_REGEX}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
